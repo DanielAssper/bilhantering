d38be97363dba593ffa9687baf657f8bba7867b0
Joshua George <joshua.george@consid.se>
2020-12-16 19:50:20 +0100

    Skapat bil modell samt sparar objektet till en json fil

--- bilhantering/MainWindow.xaml.cs
+++ bilhantering/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,18 +15,28 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace bilhantering
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public class Bil
+    {
+        public string RegNr { get; set; }
+        public string Modell { get; set; }
+        public int Vikt { get; set; }
+        public int Hk { get; set; }
+        public bool Elbil { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
-        string reg = "";
-        string modell = "";
-        int vikt;
-        int hk;
-        string elbil;
+ 
+        bool elbil;
+        List<Bil> bilar = new List<Bil>();
 
         public MainWindow()
         {
@@ -36,23 +48,38 @@ namespace bilhantering
 
             try
             {
-
-                reg = textboxReg.Text;
-                modell = textboxModell.Text;
-                vikt = int.Parse(textboxVikt.Text);
-                hk = int.Parse(textboxHästkrafter.Text);
                
-                Listview.Items.Add($"| {reg} | {modell} | {vikt}  | {hk} | {elbil} |  ");
+                Bil bil = new Bil()
+                {
+                    RegNr = textboxReg.Text,
+                    Modell = textboxModell.Text,
+                    Vikt = int.Parse(textboxVikt.Text),
+                    Hk = int.Parse(textboxHästkrafter.Text),
+                    Elbil = elbil
+                };
+
+                bilar.Add(bil);
+
+                //Serialize = Ta ett C# objekt och sedan göra det till json.
+                string jsonString = JsonConvert.SerializeObject(bilar);
+                File.WriteAllText(@"minabilar.json", jsonString);
+
+                //TODO:
+                //1. Läs från json-filen (Deserialisera). Se minabilar.json under \bilhantering\bilhantering\bin\Debug -> I debug mappen finns minabilar.json
+                //2. Iterera genom arrayen.
+                //3. Lägg till de sparade bilarna i ListView. Tips, döp om Listview till något annat. 
+                //4. Snygga till listan genom att lägga till kolumnerna Registreringsnummer, Modell, Vikt, Hästkrafter, Elbil
+
+                Listview.Items.Add($"| {bil.RegNr} | {bil.Modell} | {bil.Vikt}  | {bil.Hk} | {bil.Elbil} |  ");
+
             } catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
 
-          
-
-            
-
         }
+
+
 
         private void tabort_Click(object sender, RoutedEventArgs e)
         {
@@ -66,9 +93,7 @@ namespace bilhantering
 
         private void checkboxJa_Checked(object sender, RoutedEventArgs e)
         {
-
-            elbil = "Elbil";
-              
+            elbil = true;
         }
 
 
@@ -84,7 +109,7 @@ namespace bilhantering
 
         private void checkboxNej_Checked(object sender, RoutedEventArgs e)
         {
-            elbil = "Inte Elbil";
+            elbil = false;
         }
     }
 }
